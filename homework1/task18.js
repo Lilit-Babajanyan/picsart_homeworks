0498f6c2780f3509bb704ff49fc701efe8ee8765
Lilit Babajanyan <lilitbabajanyan@MacBook-AirLilit.local>
2026-03-04 21:47:48 +0400

    Changed version

--- homework1/task18.js
+++ homework1/task18.js
@@ -1,10 +1,11 @@
 function TypeOfValue(value){
-    if(typeof value === "object" || value === null){
+    if(typeof value === "object" ||  typeof value === "function" || value===null){
         return false;
-    }else{
+    } else {
         return true;
     }
 }
-
 let value = "hello";
 console.log(TypeOfValue(value));
+console.log(TypeOfValue(null));
+console.log(TypeOfValue(NaN));
