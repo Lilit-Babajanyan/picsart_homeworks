6981e3a60bb10dbf352aa5377f80ed6b312c943d
Lilit Babajanyan <lilitbabajanyan@MacBook-AirLilit.local>
2026-03-04 20:08:10 +0400

    Changed version

--- homework1/task12.js
+++ homework1/task12.js
@@ -1,14 +1,19 @@
 function FalsyValues(value){
-    if(value===0 || value==="" || value===null || value===undefined || value === NaN || value===-0 || value ===false){
+    if(value===0 || value==="" || value===null || value===undefined || Number.isNaN(value) || value===-0 || value ===false){
         return true;
     }else {
         return false;
     }
 
 }
+    
 console.log(FalsyValues(0));
 console.log(FalsyValues(8));
 console.log(FalsyValues(""));
+console.log(FalsyValues(null));
+console.log(FalsyValues(undefined));
+console.log(FalsyValues(false));
+console.log(FalsyValues(NaN));
 
 
 /*
