fc4375decb70fce4c012c5a3da9d93083b716737
Lilit Babajanyan <lilitbabajanyan@MacBook-AirLilit.local>
2026-03-04 20:19:26 +0400

    Changed version

--- homework1/task17.js
+++ homework1/task17.js
@@ -1,14 +1,17 @@
 function IsObject(value){
     if(typeof value === "object"){
-        if(value === null || Array.isArray(value) || typeof value === "function" ){
+        if(value === null || Array.isArray(value)){
             return false;
         }
+    }
+    if(typeof value === "function"){
+        return false;
+    }
         return true;
     }
-}
 
 console.log(IsObject({}));           
 console.log(IsObject({ a: 1 }));       
 console.log(IsObject([]));             
 console.log(IsObject(null));          
-
+console.log(IsObject(IsObject));          
