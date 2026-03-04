2c64426025d6a0d448c25a2f1e2ce2ac5c7af271
Lilit Babajanyan <lilitbabajanyan@MacBook-AirLilit.local>
2026-03-04 21:54:12 +0400

    Changed version

--- homework1/task19.js
+++ homework1/task19.js
@@ -1,5 +1,5 @@
 function SumOfNumbers(num1, num2) {
-  if (typeof num1 === "number" && typeof num2 === "number" && Number.isFinite(num1) &&Number.isFinite(num2)) {
+  if (typeof num1 === "number" && typeof num2 === "number" && Number.isFinite(num1) && Number.isFinite(num2)) {
     return num1 +num2;
   }
   return "Invalid Input";
