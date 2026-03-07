c3cada2ed59abee47136e7249ebb325ed37e8d53
Lilit Babajanyan <lilitbabajanyan@MacBook-AirLilit.local>
2026-03-08 00:18:09 +0400

    Changed version

---
+++ homework2/first_and_last.js
@@ -0,0 +1,9 @@
+function firstAndLast(arr) {
+    if (arr.length === 0) return [];           
+    return [arr[0], arr[arr.length - 1]];      
+
+}
+
+console.log(firstAndLast([1, 2, 3])); 
+console.log(firstAndLast([7]));    
+console.log(firstAndLast([]));    
