de2eb2a1063274538ad7428b55c149af96efc941
Lilit Babajanyan <lilitbabajanyan@MacBook-AirLilit.local>
2026-03-07 22:26:48 +0400

    Changed version

---
+++ homework2/sum_of_arr.js
@@ -0,0 +1,12 @@
+function sumArray(arr){
+    let sum = 0;
+    for(let i = 0; i<arr.length; i++){
+        if(typeof arr[i]!== "number"){
+            continue;
+        }
+        sum+=arr[i];
+    }
+    return sum;
+}
+let arr=[1,2,3, "hello", 1];
+console.log(sumArray(arr));
