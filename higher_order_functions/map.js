9cf27fe50a09edaa09c3ab711dbacdc4c2a32d22
Lilit Babajanyan <lilitbabajanyan@MacBook-AirLilit.local>
2026-03-25 00:47:21 +0400

    map

--- higher_order_functions/map.js
+++ higher_order_functions/map.js
@@ -3,12 +3,16 @@ let arr = [1,2,3,4];
 let res = arr.map(x=> x+2);
 console.log(res);
 */
-let newArr=[];
-function impl(arr){
-    for(let i =0; i<arr.length; i++){
-        newArr.push(arr[i]+2)
-    }
-    return newArr;
+
+function callback(array, value, i){
+    return value+2;
 }
-console.log(impl(arr));
-console.log(arr);
+
+function newArr(arr, callback){
+    let newArray=[];
+    for(let i = 0; i<arr.length; i++){
+        newArray.push(callback(arr, arr[i], i));
+    }
+    return newArray;
+}
+console.log(newArr(arr, callback));
