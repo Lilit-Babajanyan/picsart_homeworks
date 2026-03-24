98c9cda1369f26c4b6854bc150e2771c7ef3fe13
Lilit Babajanyan <lilitbabajanyan@MacBook-AirLilit.local>
2026-03-25 00:53:15 +0400

    some

--- higher_order_functions/some.js
+++ higher_order_functions/some.js
@@ -1,13 +1,18 @@
-let arr = [8,9,10];
+let arr = [8,2,10];
 /*
 console.log(arr.some(x=> x<3));
 */
-function impl(arr){
+function callback(value, i, array){
+    return value<3;
+}
+
+function impl(arr, callback){
     for(let i = 0; i<arr.length; i++){
-        if(arr[i]<3){
+        if(callback(arr[i], i, arr)){
             return true;
-    }
+        }
     }
     return false;
 }
-console.log(impl(arr))
+
+console.log(impl(arr, callback));
