16904864bc73ec244934ceb3ac756e4061b8f091
Lilit Babajanyan <lilitbabajanyan@MacBook-AirLilit.local>
2026-03-25 00:30:30 +0400

    every

--- higher_order_functions/every.js
+++ higher_order_functions/every.js
@@ -2,13 +2,17 @@ let arr = [1,2,3,4];
 /*
 console.log(arr.every(x=>x<4));
 */
+function callback(value, array, i){
+        return value<4;
+}
 
-function impl(arr){
+function impl(arr, callback){
     for(let i = 0; i<arr.length; i++){
-        if(arr[i]>=4){
+        if(!callback(arr[i], arr, i)){
             return false;
         }
     }
     return true;
 }
-console.log(impl(arr));
+
+console.log(impl(arr, callback));
