f7a8fb25ad4b6b54b07a54ab961aae858fcdc157
Lilit Babajanyan <lilitbabajanyan@MacBook-AirLilit.local>
2026-03-25 01:19:24 +0400

    indexOf

--- higher_order_functions/indexOf.js
+++ higher_order_functions/indexOf.js
@@ -1,3 +1,15 @@
 let arr = [1, 2, 3, 4, 5];
-let res = arr.indexOf(5);
-console.log(res);
+
+function callback(value, i, array){
+    return value===2;
+}
+
+function impl(arr, callback){
+    for(let i = 0; i<arr.length; i++){
+        if(callback(arr[i], i, arr)){
+            return i;
+        }
+        }
+        return -1;
+    }
+console.log(impl(arr, callback));
