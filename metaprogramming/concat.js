8ce089ae36c10dde3521de6067b760f600260973
Lilit Babajanyan <lilitbabajanyan@MacBook-AirLilit.local>
2026-04-20 23:07:38 +0400

    isConcatSpreadable

---
+++ metaprogramming/concat.js
@@ -0,0 +1,15 @@
+let digits = [1, 2, 3];
+let extraNumbers = {
+    0 : 5,
+    1 : 10,
+    length : 2,
+
+    [Symbol.isConcatSpreadable]: true
+}
+
+let bonus = [4, 5];
+bonus[Symbol.isConcatSpreadable] === false,
+
+
+console.log(digits.concat(extraNumbers));
+console.log(digits.concat(bonus));
